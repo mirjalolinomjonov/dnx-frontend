f8f2690a2170a3a242d142d687fb198be1d267de
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2026-01-22 23:01:12 +0500

    fix: text truncate

--- src/components/task/TaskDetail.vue
+++ src/components/task/TaskDetail.vue
@@ -2,7 +2,7 @@
 const list = [
   'Understanding the tools in Figma',
   'Understand the basics of making designs',
-  ' Design a mobile application with figma',
+  'Design a mobile application with figma',
 ]
 </script>
 
@@ -12,13 +12,18 @@ const list = [
       <h3 class="title-base text-secondary-500 font-semibold!">Detail Task</h3>
       <span class="text text-secondary-400">UI / UX Designer</span>
     </div>
-    <div v-for="(item, index) in list" :key="index" class="space-x-3">
+    <div v-for="(item, index) in list" :key="index" class="flex-center gap-3">
       <span
-        class="w-9 h-9 rounded-base inline-flex-center-center bg-secondary-light title-base font-semibold!"
+        class="shrink-0 w-9 h-9 rounded-base inline-flex-center-center bg-secondary-light title-base font-semibold!"
       >
         {{ index + 1 }}
       </span>
-      <span class="title-base text-secondary-500"> Design a mobile application with figma </span>
+      <span
+        :title="item"
+        class="title-base text-secondary-500 inline-block max-w-[80%] overflow-hidden text-ellipsis whitespace-nowrap"
+      >
+        {{ item }}
+      </span>
     </div>
   </div>
 </template>

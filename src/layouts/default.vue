947713e0ba789f19654060d4bad509be44633bc7
mirjalolinomjonov <mirjalolinomjonov@gmail.com>
2026-01-22 17:46:38 +0500

    feat: add default and error layout components

---
+++ src/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppMenu from '@/components/AppMenu.vue'
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <AppMenu class="shrink-0" />
+    <main class="grow min-w-0">
+      <!-- <AppHeader /> -->
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/layouts/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="grid place-items-center h-screen">
+    <div class="text-center">
+      <h1 class="text-9xl font-bold">404</h1>
+      <p class="text-3xl leading-12">Not found this page!</p>
+      <router-link class="text-2xl underline text-blue-500" to="/">Back to home</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
